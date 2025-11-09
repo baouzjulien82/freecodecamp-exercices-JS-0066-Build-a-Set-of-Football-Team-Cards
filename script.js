b6de37f61086d9fd6d48a52281e2a587c58e5902
Julien Baouz <baouzjulien@gmail.com>
2025-11-09 12:31:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
+// Objet principal: équipe de la coupe du monde 1998
+let footballTeam = {
+  team: "France",
+  year: 1998,
+  headCoach: "Aimé Jacquet",
+  players: [
+    {
+      name: "Didier Deschamps",
+      position: "midfielder",
+      isCaptain: "true"
+    },
+    {
+      name: "Zinedine Zidane",
+      position: "midfielder",
+      isCaptain: "false"
+    },
+    {
+      name: "Fabien Barthez",
+      position: "goalkeeper",
+      isCaptain: "false"
+    },
+    {
+      name: "Laurent Blanc",
+      position: "defender",
+      isCaptain: "false"
+    },
+    {
+      name: "Marcel Desailly",
+      position: "defender",
+      isCaptain: "false"
+    },
+    {
+      name: "Lilian Thuram",
+      position: "defender",
+      isCaptain: "false"
+    },
+    {
+      name: "Bixente Lizarazu",
+      position: "defender",
+      isCaptain: "false"
+    },
+    {
+      name: "Emmanuel Petit",
+      position: "midfielder",
+      isCaptain: "false"
+    },
+    {
+      name: "Youri Djorkaeff",
+      position: "midfielder",
+      isCaptain: "false"
+    },
+    {
+      name: "Stéphane Guivarc’h",
+      position: "forward",
+      isCaptain: "false"
+    },
+    {
+      name: "Thierry Henry",
+      position: "forward",
+      isCaptain: "false"
+    }
+  ]
+};
 
+// Définition des variables Team stats
+let teamName = document.getElementById('team');
+let teamYear = document.getElementById('year');
+let headCoach = document.getElementById('head-coach');
+
+// Définition de la variable Filter
+let playersFilter = document.getElementById('players');
+
+// Définition des variables player-cards
+let playerCards = document.getElementById('player-cards');
+
+// Remplissage des span Team Year et Head coach
+teamName.textContent = footballTeam.team;
+teamYear.textContent = footballTeam.year;
+headCoach.textContent = footballTeam.headCoach;
+
+// Définition et tri des cartes joueurs
